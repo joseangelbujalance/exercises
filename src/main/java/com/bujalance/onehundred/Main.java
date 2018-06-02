f7dc9e447c7d65a1797a99798e0d17ad8fae97a4
Jose Bujalance <joseanbu23@gmail.com>
2018-06-02 14:34:00 +0200

    Solutions

--- src/main/java/com/bujalance/onehundred/Main.java
+++ src/main/java/com/bujalance/onehundred/Main.java
@@ -1,28 +1,40 @@
 package com.bujalance.onehundred;
 
-import java.util.HashSet;
-import java.util.Set;
+import org.mariuszgromada.math.mxparser.Expression;
 
 public class Main {
 
     private static int TARGETED_RESULT = 100;
 
     public static void main(String[] args) {
-        String sequence = "123";
-        printAllCombinations(sequence, 1);
+        String sequence = "123456789";
+        char[] signs = new char[] {'+', '-', '_'};
+        printCombinations(1, sequence, signs, true);
     }
 
-    private static void printAllCombinations(final String sequence, int index) {
+    private static void printCombinations(int index, final String sequence, final char[] signs, final boolean onlySolutions) {
         if (index >= sequence.length()) {
-            System.out.println(sequence);
+            if (onlySolutions) {
+                if (sequenceIsSolution(sequence, TARGETED_RESULT)) {
+                    System.out.println(sequence);
+                }
+            } else {
+                System.out.println(sequence);
+            }
             return;
         }
 
         String alreadyTreated = sequence.substring(0, index);
         String toTreat = sequence.substring(index);
         int newIndex = index + 2;
-        printAllCombinations(alreadyTreated + "+" + toTreat, newIndex);
-        printAllCombinations(alreadyTreated + "-" + toTreat, newIndex);
-        printAllCombinations(alreadyTreated + "#" + toTreat, newIndex);
+        for (char sign : signs) {
+            printCombinations(newIndex, alreadyTreated + sign + toTreat, signs, onlySolutions);
+        }
+    }
+
+    private static boolean sequenceIsSolution(final String sequence, final int targetedResult) {
+        Expression expression = new Expression(sequence.replaceAll("_", ""));
+        double result = expression.calculate();
+        return result == targetedResult;
     }
 }
